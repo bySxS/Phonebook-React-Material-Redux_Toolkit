6ff44528ee9555891211657e265f091f318a85db
bySxS <88063532+bySxS@users.noreply.github.com>
2022-09-29 14:27:01 +0300

    json catch error

--- src/features/contacts/store/phonebook-api.tsx
+++ src/features/contacts/store/phonebook-api.tsx
@@ -4,7 +4,11 @@ export const fetchPhoneBook = () => {
     return new Promise<IContacts[]>((resolve, reject) => {
       setTimeout(() => {
         fetch('/phones.json')
-          .then(res => resolve(res.json()))
+          .then(res => {
+            res.json()
+              .then(value => resolve(value))
+              .catch(reason => reject(Error(`Error json in fetchPhoneBook. ${reason.message}`).message))
+          })
           .catch(reason => {
             reject(Error(`${reason.message}. Details see to log`).message)
           })
