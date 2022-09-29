8fa715c90e82a463a4736baf304c9460782bdf36
bySxS <88063532+bySxS@users.noreply.github.com>
2022-09-29 13:59:26 +0300

    Update phonebook-api.tsx

--- src/features/contacts/store/phonebook-api.tsx
+++ src/features/contacts/store/phonebook-api.tsx
@@ -3,7 +3,7 @@ import { IContacts } from '../ts/сontacts-interface'
 export const fetchPhoneBook = () => {
     return new Promise<IContacts[]>((resolve, reject) => {
       setTimeout(() => {
-        fetch('phones.json')
+        fetch('/phones.json')
           .then(res => resolve(res.json()))
           .catch(reason => {
             reject(Error(`${reason.message}. Details see to log`).message)
